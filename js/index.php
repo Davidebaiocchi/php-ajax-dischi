6a7f1972fcc1adf3fbfda82b9640d7cdd4071662
davide <davide.baiocchi97@gmail.com>
2021-07-29 16:09:26 +0200

    end project

--- js/index.php
+++ js/index.php
@@ -9,12 +9,27 @@
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <!-- link axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- link css -->
+    <link rel="stylesheet" href="../css/style2.css">
 </head>
 <body>
-    
-    <div id="app">
 
+    <header></header>
+
+    <main>
+    <div id="app">
+        <ul>
+            <li v-for="disco in dischi">
+                <img :src="disco.poster" alt="">
+                <div class="description">
+                <h2> {{ disco.title }} </h2>
+                <h3> {{ disco.author }} </h3>
+                <h4> {{ disco.year }} </h4>
+                </div>
+            </li>
+        </ul>
     </div>
+    </main>
 
 <script src="script.js"></script>
 </body>
--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-            url: 'api/api-dischi.php'
+            url: '../api/api-dischi.php',
+            dischi : ''
         },
         mounted(){
             axios
                 .get(this.url)
                 .then(response => {
-                    console.log(response);
+                    this.dischi = response.data;
+                    console.log(this.dischi);
                 })
                 .catch(error => {
                     console.log(error);
